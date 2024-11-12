d6b123f758a37c490cb829376abdb26aef6c6279
Nadira3 <paitanun35@gmail.com>
2024-11-12 07:32:38 +0100

    test the GithubOrgClient.public_repos method in an integration test. That means that we will only mock code that sends external requests.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,7 +6,11 @@
 import unittest
 from unittest.mock import MagicMock, patch
 from client import GithubOrgClient
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
+from fixtures import TEST_PAYLOAD
+
+# Unpacking the payloads for easier use
+org_payload, repos_payload, expected_repos, apache2_repos = TEST_PAYLOAD[0]
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -112,11 +116,30 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, expected)
 
 
-# Run the tests
-if __name__ == '__main__':
-    unittest.main()
+@parameterized_class([
+    {"org_payload": org_payload, "repos_payload": repos_payload,
+     "expected_repos": expected_repos, "apache2_repos": apache2_repos}
+])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Integration tests for GithubOrgClient.public_repos method."""
 
+    @classmethod
+    def setUpClass(cls):
+        """Start patching requests.get with expected JSON responses."""
+        cls.get_patcher = patch('requests.get')
+        cls.mock_get = cls.get_patcher.start()
 
-# Run the tests
-if __name__ == '__main__':
-    unittest.main()
+        # Mocking the requests.get().json() to return appropriate data
+        def side_effect(url):
+            if url == GithubOrgClient.ORG_URL.format(org="google"):
+                return org_payload
+            elif url == org_payload["repos_url"]:
+                return repos_payload
+            return None
+
+        cls.mock_get.return_value.json.side_effect = side_effect
+
+    @classmethod
+    def tearDownClass(cls):
+        """Stop patching."""
+        cls.get_patcher.stop()
